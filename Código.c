8730fff6b92eb889c8426d50f0b8b9c70f508fe7
Daniel Mendes <danieltellesmendes@gmail.com>
2026-05-07 16:57:23 +0100

    feat: useful comms

--- Código.c
+++ Código.c
@@ -3,3 +3,17 @@
 int main(){
     return 0;
 }
+
+//yo wassup, vou ver se faço uma estrutura ou outra do projeto
+//das sempre git pull para teres sempre o ultimo estado de commits do repo
+// commit para gravar e push para mandares para o repo de todos (restando os outros fazerem pull)
+// se criares branches tens de dar push para o people ver as branches
+
+
+
+//crias branches com "git checkout -b {nome}" ou "git branch {nome}", a diferença é que o "checkout" manda te logo para a branch q criaste, a "branch" apenas cria mas dps tens de fazer checkout para mudar para a mesma
+//mudas de branch com "git checkout {nome}"
+//das delete de branches com "git branch -D {nome}"
+//ves as branches existentes com "git branch -a" (aquela com * ao lado é onde tu tas)
+
+//é isso, espero que ajude :)
